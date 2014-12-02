b613ef822f8c91b2ceceaaa912f640c01186564b
hieuchuyentin <hieuchuyentin@7e147d0b-7995-bbb1-5ceb-ec9cc818d64f>
2014-12-02 17:33:24 +0000

    Code hoàn thiện phần test, còn lỗi bắt sai nhiều

--- BTL_HM/BTL_HM/BTL_HMDialog.h
+++ BTL_HM/BTL_HM/BTL_HMDialog.h
@@ -7,8 +7,8 @@
 
 #define SZ_NAME_FILE_SPAM_TRAIN		_T("train_SPAM.ems")
 #define SZ_NAME_FILE_NORMAL_TRAIN	_T("train_GEN.ems")
-#define SZ_NAME_FILE_SPAM_TEST		_T("MailTestSpam.txt")
-#define SZ_NAME_FILE_NORMAL_TEST	_T("MailTestNormal.txt")
+#define SZ_NAME_FILE_SPAM_TEST		_T("test_SPAM.ems")
+#define SZ_NAME_FILE_NORMAL_TEST	_T("test_GEN.ems")
 
 #define MY_MAX_PATH	1024
 #define TEXT_BEGIN_TAG	_T("<TEXT_NORMAL>")
@@ -38,6 +38,8 @@ public:
 
 	CString m_sFolderTrain;
 	CString m_sFolderTest;
+	CStringList m_listMailNormal;
+	CStringList m_listMailSpam;
 	CStringList m_listMailTrainSpam;
 	CStringList m_listMailTrainNormal;
 	CStringList m_listMailTestSpam;
@@ -48,27 +50,32 @@ public:
 	CStringList m_listKey;
 	TOKEN_LIST m_listProbabilityKeyOnSpam;
 	TOKEN_LIST m_listProbabilityKeyOnNormal;
+	TOKEN_SCORE_LIST m_listTokenScore;
+
 	int m_nKeyNumber;
 	ProcessWord stopword;
 
 public:
 	afx_msg void OnBnClickedButton1();
 	afx_msg void OnBnClickedButton2();
-	afx_msg void OnBnClickedButton3();
 
 	void GetMail(__in CString sPath, __out CStringList& sListMail);
 	//void GetFrequency(CStringList& listMail, __out TOKEN_LIST& TOKEN_INFO);
 	void GetKey(TOKEN_LIST listNormal[], TOKEN_LIST listSpam[], __out CStringList& listKey);
 	void GetAppearOneMail(CStringList& listKey, CString sMail, __out NUMBER_LIST& listFrequency);
-	void CalculateProbability(CStringList& listMail, CStringList& listKey, __out TOKEN_LIST& listProbability);
+	/*void CalculateProbability(CStringList& listMail, CStringList& listKey, __out TOKEN_LIST& listProbability);*/
+	void CalculateProbability(CStringList& listMailHam, CStringList& listMailSpam, CStringList& listKey, __out TOKEN_SCORE_LIST& listProbability);
+	void GroupMail(CStringList& listMail, CStringList& listMailTrain, CStringList& listMailTest);
 	void CountWord(CStringList& listMail, TOKEN_LIST listToken[]);
 	int HashString(CString sToken);
 	void StemWord(TOKEN_LIST listToken[]);
 
 	CEdit textFolderTrain;
 	CEdit textFolderTest;
+	CEdit* m_edtResult;
 	CButton btTrain;
 	CButton btTest;
 	CButton btEvaluate;
-
+	
+	CListCtrl* m_lvTokens;
 };
