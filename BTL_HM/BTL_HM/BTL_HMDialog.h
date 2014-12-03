c683fbbb30d9d35accdd55577a63d8b04db0c90a
hieuchuyentin@gmail.com <hieuchuyentin@gmail.com@7e147d0b-7995-bbb1-5ceb-ec9cc818d64f>
2014-12-03 18:19:42 +0000

    Đưa thêm cách tìm key mới và tính xác suất mới. Chưa đạt được hiệu quả, vẫn hậu thuẫn cho việc phân loại một mail thành mail rác.

--- BTL_HM/BTL_HM/BTL_HMDialog.h
+++ BTL_HM/BTL_HM/BTL_HMDialog.h
@@ -5,8 +5,8 @@
 
 // CBTL_HMDialog dialog
 
-#define SZ_NAME_FILE_SPAM_TRAIN		_T("train_SPAM.ems")
-#define SZ_NAME_FILE_NORMAL_TRAIN	_T("train_GEN.ems")
+#define SZ_NAME_FILE_SPAM_TRAIN		_T("test_SPAM.ems")
+#define SZ_NAME_FILE_NORMAL_TRAIN	_T("test_GEN.ems")
 #define SZ_NAME_FILE_SPAM_TEST		_T("test_SPAM.ems")
 #define SZ_NAME_FILE_NORMAL_TEST	_T("test_GEN.ems")
 
@@ -62,9 +62,12 @@ public:
 	void GetMail(__in CString sPath, __out CStringList& sListMail);
 	//void GetFrequency(CStringList& listMail, __out TOKEN_LIST& TOKEN_INFO);
 	void GetKey(TOKEN_LIST listNormal[], TOKEN_LIST listSpam[], __out CStringList& listKey);
+	void GetKeyNew(TOKEN_LIST listNormal[], TOKEN_LIST listSpam[], __out CStringList& listKey);
+	
 	void GetAppearOneMail(CStringList& listKey, CString sMail, __out NUMBER_LIST& listFrequency);
 	/*void CalculateProbability(CStringList& listMail, CStringList& listKey, __out TOKEN_LIST& listProbability);*/
-	void CalculateProbability(CStringList& listMailHam, CStringList& listMailSpam, CStringList& listKey, __out TOKEN_SCORE_LIST& listProbability);
+	void CalculateProbability(CStringList& listMailHam, CStringList& listMailSpam,CStringList& listKey, __out TOKEN_SCORE_LIST& listProbability);
+	void CalculateProbabilityNew(TOKEN_LIST listTokenNormal[], TOKEN_LIST listTokenSpam[], CStringList& listKey, __out TOKEN_SCORE_LIST& listProbability);
 	void GroupMail(CStringList& listMail, CStringList& listMailTrain, CStringList& listMailTest);
 	void CountWord(CStringList& listMail, TOKEN_LIST listToken[]);
 	int HashString(CString sToken);
