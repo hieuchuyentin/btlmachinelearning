6ad2e7bb998b9d3f25c0e5af93beb13476edc585
hieuchuyentin@gmail.com <hieuchuyentin@gmail.com@7e147d0b-7995-bbb1-5ceb-ec9cc818d64f>
2014-12-04 07:03:30 +0000

    Thêm code cho tính năng đảo mail. Hàm đảo mail lỗi.

--- trunk/BTL_HM/BTL_HM/BTL_HMDialog.h
+++ trunk/BTL_HM/BTL_HM/BTL_HMDialog.h
@@ -5,8 +5,8 @@
 
 // CBTL_HMDialog dialog
 
-#define SZ_NAME_FILE_SPAM_TRAIN		_T("train_SPAM.ems")
-#define SZ_NAME_FILE_NORMAL_TRAIN	_T("train_GEN.ems")
+#define SZ_NAME_FILE_SPAM_TRAIN		_T("test_SPAM.ems")
+#define SZ_NAME_FILE_NORMAL_TRAIN	_T("test_GEN.ems")
 #define SZ_NAME_FILE_SPAM_TEST		_T("test_SPAM.ems")
 #define SZ_NAME_FILE_NORMAL_TEST	_T("test_GEN.ems")
 
@@ -60,19 +60,20 @@ public:
 	afx_msg void OnBnClickedButton2();
 
 	void GetMail(__in CString sPath, __out CStringList& sListMail);
-	//void GetFrequency(CStringList& listMail, __out TOKEN_LIST& TOKEN_INFO);
 	void GetKey(TOKEN_LIST listNormal[], TOKEN_LIST listSpam[], __out CStringList& listKey);
 	void GetKeyNew(TOKEN_LIST listNormal[], TOKEN_LIST listSpam[], __out CStringList& listKey);
 	void VerifyKey(CStringList& listKey);
 
 	void GetAppearOneMail(CStringList& listKey, CString sMail, __out NUMBER_LIST& listFrequency);
-	/*void CalculateProbability(CStringList& listMail, CStringList& listKey, __out TOKEN_LIST& listProbability);*/
 	void CalculateProbability(CStringList& listMailHam, CStringList& listMailSpam,CStringList& listKey, __out TOKEN_SCORE_LIST& listProbability);
 	void CalculateProbabilityNew(TOKEN_LIST listTokenNormal[], TOKEN_LIST listTokenSpam[], CStringList& listKey, __out TOKEN_SCORE_LIST& listProbability);
 	void GroupMail(CStringList& listMail, CStringList& listMailTrain, CStringList& listMailTest);
 	void CountWord(CStringList& listMail, TOKEN_LIST listToken[]);
 	int HashString(CString sToken);
 	void StemWord(TOKEN_LIST listToken[]);
+	
+	void Train();
+	void Test();
 
 	CEdit textFolderTrain;
 	CEdit textFolderTest;
@@ -82,4 +83,5 @@ public:
 	CButton btEvaluate;
 	
 	CListCtrl* m_lvTokens;
+	afx_msg void OnBnClickedButton3();
 };
